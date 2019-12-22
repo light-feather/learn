7d70ae9499f4c60bb359ad7fb477ae73f8681c38
羽墨 <46095817+light-feather@users.noreply.github.com>
2019-12-22 15:40:24 +0800

    Debug
    进行Debug快捷键的学习和试验

---
+++ IDEA/debug/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public void foo() {
+
+    }
+
+    public void test() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I am testing 'ctrl+shift+F9'.");
+        System.out.println("I am testing the value of args[0]: "+args[0]);
+    }
+}
---
+++ IDEA/debug/DemoTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void foo(){
+        //此快捷键已改为：ctrl+shift+F10
+        System.out.println("I am testing running foo() with 'shift+F9'.");
+    }
+
+    @Test
+    public void bar(){
+        System.out.println("I am testing running bar() with 'shift+F9'.");
+    }
+}
